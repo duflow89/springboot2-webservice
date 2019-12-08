a229d6688f87ef2705dcccfdbe75163db0912b11
duflow89 <lehejo66@gmail.com>
2019-12-09 00:27:38 +0900

    Run & DTO

---
+++ src/main/java/com/duflow89/book/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.duflow89.book.web;
+
+import com.duflow89.book.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
---
+++ src/main/java/com/duflow89/book/web/WebController.java
@@ -0,0 +1,16 @@
+package com.duflow89.book.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+
+
+}
