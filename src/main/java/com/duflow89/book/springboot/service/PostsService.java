e1d101f510fc1c793e34945728cf154230d2515d
HeeJoongLee <lehejo66@gmail.com>
2019-12-10 11:52:31 +0900

    Spring Data JPA & Post CRUD

---
+++ src/main/java/com/duflow89/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.duflow89.book.springboot.service;
+
+import com.duflow89.book.springboot.domain.posts.PostsRepository;
+import com.duflow89.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
