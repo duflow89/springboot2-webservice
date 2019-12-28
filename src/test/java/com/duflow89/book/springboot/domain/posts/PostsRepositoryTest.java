b6681256177c8a8c76b6b0eb88fddccc57aaeb3e
duflow89 <lehejo66@gmail.com>
2019-12-28 14:53:43 +0900

    BaseTimeEntity 추가

--- src/test/java/com/duflow89/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/duflow89/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.duflow89.book.springboot.domain.posts;
 
+import javafx.geometry.Pos;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,4 +45,30 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void baseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019,12,28,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("duflow89")
+                .build()
+        );
+
+        //when
+        Posts posts = postsRepository.findAll().get(0);
+
+        //then
+        String result = "createdDate: " + posts.getCreatedDate()
+                + "modifiedDate: " + posts.getModifiedDate();
+
+        System.out.println(result);
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+    }
+
 }
