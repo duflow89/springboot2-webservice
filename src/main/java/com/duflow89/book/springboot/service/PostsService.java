76c6e914ae96d9e0bb124cd37a26bd9259834bbc
duflow89 <lehejo66@gmail.com>
2020-01-05 23:46:54 +0900

    게시글 목록 구현

--- src/main/java/com/duflow89/book/springboot/service/PostsService.java
+++ src/main/java/com/duflow89/book/springboot/service/PostsService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,9 +33,17 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
