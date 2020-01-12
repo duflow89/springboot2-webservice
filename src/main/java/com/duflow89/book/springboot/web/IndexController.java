fc53cd39e026bb7fc8ba9578e89c65db4f90bbc4
duflow89 <lehejo66@gmail.com>
2020-01-12 14:59:07 +0900

    게시글 수정 기능 구현

--- src/main/java/com/duflow89/book/springboot/web/IndexController.java
+++ src/main/java/com/duflow89/book/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.duflow89.book.springboot.web;
 
 import com.duflow89.book.springboot.service.PostsService;
+import com.duflow89.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,9 +21,18 @@ public class IndexController {
         return "index";
     }
 
+
     @GetMapping("/posts/save")
     public String postsSave() {
-        return "/posts-save";
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
-var index = {
+var main = {
     init: function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
+        });
+        $('#btn-update').on('click', function () {
+            _this.update();
         });
     },
     save: function () {
@@ -26,7 +29,28 @@ var index = {
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
 };
 
-index.init();
+main.init();
