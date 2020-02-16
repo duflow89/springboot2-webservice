8b88381f6a0e633580b562e816e2cc8512db93bf
duflow89 <lehejo66@gmail.com>
2020-02-16 23:54:00 +0900

    네이버 로그인

--- src/main/java/com/duflow89/book/springboot/web/IndexController.java
+++ src/main/java/com/duflow89/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.duflow89.book.springboot.web;
 
+import com.duflow89.book.springboot.config.auth.LoginUser;
 import com.duflow89.book.springboot.config.auth.dto.SessionUser;
 import com.duflow89.book.springboot.domain.user.User;
 import com.duflow89.book.springboot.service.PostsService;
@@ -21,10 +22,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model,
+                        @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
