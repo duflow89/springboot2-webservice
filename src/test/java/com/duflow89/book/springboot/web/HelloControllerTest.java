6b2f0e3ca4bc99d988fb79d284d7a4f037025cdc
duflow89 <lehejo66@gmail.com>
2020-03-05 00:19:49 +0900

    Spring Security Test

--- src/test/java/com/duflow89/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/duflow89/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.duflow89.book.springboot.web;
 
+import com.duflow89.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello_리턴됨() throws Exception {
         String hello = "hello";
@@ -29,6 +38,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_리턴됨() throws Exception {
         String name = "hello";
